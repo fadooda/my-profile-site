a6e2de64766defd9d971b35014b52d1532efe059
fadooda <fadyibrahim37@gmail.com>
2020-12-30 11:23:21 -0500

    Added comments for the landingpage

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -7,6 +7,8 @@ class LandingPage extends Component{
         return(
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className='landing-grid'>
+
+                   {/* set up the home page with the profile image center and skills/font-awesome links underneath */}
                     <Cell col={12}>
                         <img
                         src={ProfileImg}
@@ -19,32 +21,33 @@ class LandingPage extends Component{
                             </h1>
                             <hr/>
                             <p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoDB</p>
+                            
                             <div className="social-links">
+                              {/* LinkedIn */}
+                              <a href="https://www.linkedin.com/in/fady-ibrahim-b68b0a59/" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-linkedin-square" aria-hidden="true" />
+                              </a>
 
-                            {/* LinkedIn */}
-                            <a href="https://www.linkedin.com/in/fady-ibrahim-b68b0a59/" rel="noopener noreferrer" target="_blank">
-                              <i className="fa fa-linkedin-square" aria-hidden="true" />
-                            </a>
+                              {/* Github */}
+                              <a href="https://github.com/fadooda" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github-square" aria-hidden="true" />
+                              </a>
 
-                            {/* Github */}
-                            <a href="https://github.com/fadooda" rel="noopener noreferrer" target="_blank">
-                              <i className="fa fa-github-square" aria-hidden="true" />
-                            </a>
+                              {/* HackerRank */}
+                              <a href="https://www.hackerrank.com/fadyibrahim37" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-user-secret" aria-hidden="true" />
+                              </a>
 
-                            {/* HackerRank */}
-                            <a href="https://www.hackerrank.com/fadyibrahim37" rel="noopener noreferrer" target="_blank">
-                              <i className="fa fa-user-secret" aria-hidden="true" />
-                            </a>
-
-                            {/* LeetCode */}
-                            <a href="https://leetcode.com/fadyI" rel="noopener noreferrer" target="_blank">
-                              <i className="fa fa-code" aria-hidden="true" />
-                            </a>
-
+                              {/* LeetCode */}
+                              <a href="https://leetcode.com/fadyI" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-code" aria-hidden="true" />
+                              </a>
                             </div>
                         </div>
                     </Cell>
                 </Grid>
+
+                 {/* set and style footer */}
                 <div className='footer'>
                   <p>
                       <strong>
