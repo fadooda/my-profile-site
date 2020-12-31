242877c752448be60837369b38451366ac17f814
fadooda <fadyibrahim37@gmail.com>
2020-12-30 22:12:41 -0500

    Added comments for the skill

--- src/components/skills.js
+++ src/components/skills.js
@@ -6,20 +6,20 @@ class Skills extends Component{
     render(){
         return(
             <Grid>
-                {/* Set up the respective resume image and the styling */}
+                {/*Display the prop skill */}
                 <Cell col={1}>
                     <div >
                         {this.props.skill}
                     </div>
                 </Cell>
 
+                {/*Display the progress bar and set the value to the prop progress */}
                 <Cell col={11}>
                     <div >
                         <ProgressBar style={{margin: 'auto', width:'75%'}}
                         progress={this.props.progress}/>
                     </div>
                 </Cell>
-
             </Grid>
         )
     }
