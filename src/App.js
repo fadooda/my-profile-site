fc0d26087236c51103b28442d1d243580e48c30e
fadooda <fadyibrahim37@gmail.com>
2020-12-30 22:09:54 -0500

    Refactored code

--- src/App.js
+++ src/App.js
@@ -4,39 +4,37 @@ import {Layout,Header,Navigation,Drawer,Content}  from 'react-mdl'
 import ProfileRoutes from './components/profileRoutes'
 import {Link} from 'react-router-dom'
 
+
 class App extends React.Component {
-
-
-
+  
   render()
   {
     return (
       <div className="App">
-<div className="demo-big-content">
-    <Layout>
-    <Header className='header-color' title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Fady Ibrahim</Link>} scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Fady Ibrahim</Link>} scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <ProfileRoutes/>
-        </Content>
-    </Layout>
-</div>
-
+        <div className="demo-big-content">
+          <Layout>
+            <Header className='header-color' title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Fady Ibrahim</Link>} scroll>
+              <Navigation>
+                  <Link to="/resume">Resume</Link>
+                  <Link to="/aboutme">About me</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/contact">Contact</Link>
+              </Navigation>
+            </Header>
+            <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Fady Ibrahim</Link>} scroll>
+                <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+                <ProfileRoutes/>
+            </Content>
+          </Layout>
+        </div>
       </div>
     );
   }
