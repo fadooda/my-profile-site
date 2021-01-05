8501956a7d09798c47042b21f64f120079db29d0
fadooda <fadyibrahim37@gmail.com>
2021-01-05 06:54:20 -0500

    cleaned and refactored codebase

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,43 +12,50 @@ class Project extends Component{
         }
       }
 
+      projectCardItem(text,github,livesite,img){
+        return(
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: 'purple', height: '200px', background: `url(${img}) center / cover`}}/>
+                    {/* Set up project information*/}
+                    <CardText>
+                        {text}
+                    </CardText>
+
+                    {/* Set up respective links to the github repository and the live site ---- game-site-github-repo && live-site*/}
+                    <CardActions border>
+                        {(github)? 
+                            <a href={`${github}`}  rel="noopener noreferrer" target="_blank">
+                                <Button colored >github</Button>
+                            </a>
+                            :
+                            null
+                        }
+                        {(livesite)? 
+                            <a href={`${livesite}`}  rel="noopener noreferrer" target="_blank">
+                                <Button colored >live site</Button>
+                            </a>
+                            :
+                            null
+                        }
+                    </CardActions>
+                </Card>
+        )
+      }
+
       toggleCategories(){
         if(this.state.activeTab===0){
             return(
                 <div className="projects-grid">
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color: 'purple', height: '176px', background: `url(${TickTactToeImg}) center / cover`}}/>
-                        {/* Set up project information*/}
-                        <CardText>
-                            An interactive online gaming site to showcase the power of websockets!! Grab a friend come on over and play!
-                        </CardText>
-
-                        {/* Set up respective links to the github repository and the live site ---- game-site-github-repo && live-site*/}
-                        <CardActions border>
-                            <a href="https://github.com/fadooda/game-site-react-app"  rel="noopener noreferrer" target="_blank">
-                                <Button colored >github</Button>
-                            </a>
-                            <a href="https://free-games-online.netlify.app/"  rel="noopener noreferrer" target="_blank">
-                                <Button colored >live site</Button>
-                            </a>
-                        </CardActions>
-                    </Card>
-
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color:  'black', height: '176px', background: `url(${profileProjectReact}) center / cover`}}/>
-                        
-                        <CardText>
-                            Want to know how to make a portfolio site?
-                            Check out the github link below for the codebase repository - This portfolio website.
-                        </CardText>
-
-                        {/* Set up respective link to the github repository ---- profile-site-github-server-repo*/}
-                        <CardActions border>
-                            <a href="https://github.com/fadooda/my-profile-site"  rel="noopener noreferrer" target="_blank">
-                                <Button colored >github</Button>
-                            </a>
-                        </CardActions>
-                    </Card>
+                    {this.projectCardItem('An interactive online gaming site to showcase the power of websockets!! Grab a friend come on over and play!',
+                        'https://github.com/fadooda/game-site-react-app',
+                        'https://free-games-online.netlify.app/',
+                        TickTactToeImg)}
+           
+                    {this.projectCardItem('Want to know how to make a portfolio site?  Check out the github link below for the codebase repository - This portfolio website.',
+                        'https://github.com/fadooda/my-profile-site',
+                        null,
+                        profileProjectReact)}
+           
 
                     {/* set and style footer */}
                     <div className='footer'>
@@ -64,47 +71,20 @@ class Project extends Component{
         }else if(this.state.activeTab===1){
             return(
                 <div className="projects-grid">
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color: 'black', height: '176px', background: `url(${nodeJsImg}) center / cover`}}>Authentication-Server</CardTitle>
-                        <CardText>
-                            Check out the node server for authenticating users!
-                        </CardText>
-                        
-                        {/* Set up respective link to the github repository  ---- authentication-github-server-repo*/}
-                        <CardActions border>
-                            <a href="https://github.com/fadooda/game-site-authenticate-users-app"  rel="noopener noreferrer" target="_blank">
-                                <Button colored >github</Button>
-                            </a>
-                        </CardActions>
-                    </Card>
-                    
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color:  'black', height: '176px', background: `url(${nodeJsImg}) center / cover`}}>TicTacToe-Server</CardTitle>
-                        <CardText>
-                            Interested in finding out how the tictactoe websockets work? Check out this server!
-                        </CardText>
+                    {this.projectCardItem('Check out the node server for authenticating users!',
+                        'https://github.com/fadooda/game-site-authenticate-users-app',
+                        null,
+                        nodeJsImg)}
 
-                        {/* Set up respective link to the github repository ---- tictactoe-github-server-repo*/}    
-                        <CardActions border>
-                            <a href="https://github.com/fadooda/game-site-ticktactoe-app"  rel="noopener noreferrer" target="_blank">
-                                <Button colored >github</Button>
-                            </a>
-                        </CardActions>
-                    </Card>
-                    
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color:  'black', height: '176px', background: `url(${nodeJsImg}) center / cover`}}>Chat-Server</CardTitle>
-                        <CardText>
-                            Want to know how to make a facebook-messaging service replica? Look no further! Check out this server!
-                        </CardText>
-                        
-                        {/* Set up respective link to the github repository ---- chat-github-server-repo*/}
-                        <CardActions border>
-                            <a href="https://github.com/fadooda/game-site-chat-app"  rel="noopener noreferrer" target="_blank">
-                                <Button colored >github</Button>
-                            </a>
-                        </CardActions>
-                    </Card>
+                    {this.projectCardItem('Interested in finding out how the tictactoe websockets work? Check out this server!',
+                        'https://github.com/fadooda/game-site-ticktactoe-app',
+                        null,
+                        nodeJsImg)}
+
+                    {this.projectCardItem('Want to know how to make a facebook-messaging service replica? Look no further! Check out this server!',
+                        'https://github.com/fadooda/game-site-chat-app',
+                        null,
+                        nodeJsImg)}
 
                     {/* set and style footer */}
                     <div className='footer'>
