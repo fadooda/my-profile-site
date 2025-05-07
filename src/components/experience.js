d7b407de10808d645d825e6bbadb65f1998bc8f6
Fady Ibrahim <fadyibrahim37@gmail.com>
2025-05-07 11:36:44 -0400

    fix for experience not showing company name, task four and task five.

--- src/components/experience.js
+++ src/components/experience.js
@@ -16,12 +16,13 @@ class Experience extends Component{
 
                  {/*Display the job experience content */}
                 <Cell col={8}>
-                    <h4 style={{marginTop: '0px'}}>{this.props.jobName}</h4>
-                    {(this.props.taskone)? <p> <i className="fa fa-star" aria-hidden="true" style={{paddingRight: '1em'}}/>{this.props.taskone}</p> : null}
-                    {(this.props.tasktwo)? <p> <i className="fa fa-star" aria-hidden="true" style={{paddingRight: '1em'}} />{this.props.tasktwo}</p> : null}
-                    {(this.props.taskthree)? <p> <i className="fa fa-star" aria-hidden="true" style={{paddingRight: '1em'}}/>{this.props.taskthree}</p> : null}
-                    {(this.taskfour)? <p> <i className="fa fa-star" aria-hidden="true" style={{paddingRight: '1em'}}/>{this.taskfour}</p> : null}
-                    {(this.taskfive)? <p> <i className="fa fa-star" aria-hidden="true" style={{paddingRight: '1em'}}/>{this.taskfive}</p> : null}
+                <h4 style={{ marginTop: '0px' }}>{this.props.company}</h4>
+                    {this.props.jobName && <h5>{this.props.jobName}</h5>}
+                    {this.props.taskone && <p><i className="fa fa-star" aria-hidden="true" style={{ paddingRight: '1em' }} />{this.props.taskone}</p>}
+                    {this.props.tasktwo && <p><i className="fa fa-star" aria-hidden="true" style={{ paddingRight: '1em' }} />{this.props.tasktwo}</p>}
+                    {this.props.taskthree && <p><i className="fa fa-star" aria-hidden="true" style={{ paddingRight: '1em' }} />{this.props.taskthree}</p>}
+                    {this.props.taskfour && <p><i className="fa fa-star" aria-hidden="true" style={{ paddingRight: '1em' }} />{this.props.taskfour}</p>}
+                    {this.props.taskfive && <p><i className="fa fa-star" aria-hidden="true" style={{ paddingRight: '1em' }} />{this.props.taskfive}</p>}
                 </Cell>
             </Grid>
         )
